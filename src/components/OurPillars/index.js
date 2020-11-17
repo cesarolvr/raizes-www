a3816ad7d1b1a872a198e509ceabf1212414daba
Cesar <cesar2012oliveira@gmail.com>
2020-11-17 11:55:03 -0300

    :lipstick: our pillars section finished

---
+++ src/components/OurPillars/index.js
@@ -0,0 +1,48 @@
+import React from "react"
+
+// Styles
+import "./OurPillars.scss"
+
+// Assets
+import pillar1 from "../../images/pillar-1.svg"
+import pillar2 from "../../images/pillar-2.svg"
+import pillar3 from "../../images/pillar-3.svg"
+
+const OurPillars = () => {
+  return (
+    <section className="section ourpillars-section">
+      <div className="container">
+        <h2 className="title">Nossos pilares</h2>
+        <hr className="divider" />
+        <ol className="pillars-list">
+          <li className="item">
+            <img className="icon" src={pillar1} />
+            <p className="title">Segurança</p>
+            <h5 className="description">
+              Trabalhamos com processos rigorosos de análise e uma sólida
+              política de crédito
+            </h5>
+          </li>
+          <li className="item">
+            <img className="icon" src={pillar2} />
+            <p className="title">Inteligência</p>
+            <h5 className="description">
+              Contamos com um time de profissionais qualificados e experientes
+              no mercado financeiro
+            </h5>
+          </li>
+          <li className="item">
+            <img className="icon" src={pillar3} />
+            <p className="title">Tecnologia</p>
+            <h5 className="description">
+              Nosso processo garante soluções customizadas, rápidas e eficazes
+              para seu negócio
+            </h5>
+          </li>
+        </ol>
+      </div>
+    </section>
+  )
+}
+
+export default OurPillars
---
+++ src/pages/quem-somos.js
@@ -0,0 +1,56 @@
+import React from "react"
+
+// Components
+import Layout from "../components/Layout"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import Footer from "../components/Footer"
+import SEO from "../components/Seo"
+import Contact from "../components/Contact"
+import Numbers from "../components/Numbers"
+import OurPillars from "../components/OurPillars"
+
+// Assets
+import backgroundPath from "../images/hero-quem-somos.jpg"
+
+// Styles
+import "../styles/pages/Quem-somos.scss"
+
+const Investors = () => {
+  return (
+    <Layout className="quem-somos">
+      <SEO title="Quem Somos" />
+      <Header />
+      <Hero
+        backgroundPath={backgroundPath}
+        title="Nossa história"
+        subtitle="Quem Somos"
+        description="Invista em um fundo sólido com rentabilidade acima da média do mercado"
+      />
+      <article className="article">
+        <div className="container">
+          <p>
+            Nossa história se deu inicio em meados 2014 quando o Brasil se
+            encontrava em difícil situacao politica e economica. Havia
+            expecatitiva de grande retracao em nosso PIB com reducao na
+            capacidade de credito para as pequenas e medias empresas, era
+            evidente e inevitável o credit crunch em nossa economia abrindo
+            espaco e oportunidades para novas empresas de credito. Iniciamos com
+            estrutura e profissionais experientes advindos do mercado
+            financeiro, aliando tecnológia e conhecimento. Nos envolvemos em
+            todos os processos, entregando soluções customizadas, rápidas e
+            eficazes para nossos clientes. A diligência rigorosa em nossos
+            investimentos proporciona segurança aos investidores, o que
+            contribui para retornos acima da média do mercado.
+          </p>
+        </div>
+      </article>
+      <OurPillars />
+      <Numbers />
+      <Contact />
+      <Footer />
+    </Layout>
+  )
+}
+
+export default Investors
