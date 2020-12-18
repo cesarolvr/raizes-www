75b7faa03c2af3ae43aacac259366c48c770a28f
Cesar <cesar2012oliveira@gmail.com>
2020-12-18 20:28:41 -0300

    :chart_with_upwards_trend: updating ga

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -11,7 +11,7 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 // Utils
-import isClient from "../../utils/isClient"
+import isClient from '../../utils/isClient'
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -74,17 +74,18 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
     >
       {isClient() && (
-        <>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-GY1ZDBKK45"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-GY1ZDBKK45');
-          </script>
-        </>
+        <script>
+          {(function (w, d, s, l, i) {
+            w[l] = w[l] || []
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : ""
+            j.async = true
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+            f.parentNode.insertBefore(j, f)
+          })(window, document, "script", "dataLayer", "GTM-P248NMQ")}
+        </script>
       )}
     </Helmet>
   )
